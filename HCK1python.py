ddbd873ddf172689cdd7d7e5bdb478451ce17511
mukesh <kumarmukesh789123@gmail.com>
2024-01-20 10:30:38 +0500

    first commit

---
+++ HCK1python.py
@@ -0,0 +1,12 @@
+# n= int(input('Enter your numbers: '))
+# if n%2!=0:
+#     print('weired')
+# elif 2<=n<=5:
+#     print('not weired')
+# elif 6<=n<20:
+#     print('even weired')
+# else:
+#     print('weired')
+#
+#
+#
