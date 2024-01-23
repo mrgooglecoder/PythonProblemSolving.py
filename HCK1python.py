d3c73443308adc6f5a5287985933badfe707c5b5
Mukesh Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2024-01-23 23:05:39 +0500

    Update HCK1python.py

--- HCK1python.py
+++ HCK1python.py
@@ -1,5 +1,6 @@
 
-# Write a Python program to calculate the factorial of a given number.
+#Challenge No1#
+#Write a Python program to calculate the factorial of a given number.
 # recursion
 
 def factorial_num(n):
@@ -26,14 +27,44 @@ else:
 
 
 
+
+
+
+
+
+
+#Challenge No2#
 def is_plandrome(str):
-#     str=str.replace(" ","").lower()
-#     return str==str[::-1]   #step slicing method
-# 
-# 
-# word =input('enter your word / number: ')
-# if is_plandrome(word):
-#     print(f" This {word} is a plandrome ")
-# 
-# else:
-#     print("no its not plandrome")
+    str=str.replace(" ","").lower()
+    return str==str[::-1]   #step slicing method
+
+
+word =input('enter your word / number: ')
+if is_plandrome(word):
+    print(f" This {word} is a plandrome ")
+
+else:
+    print("no its not plandrome")
+
+
+
+
+
+
+
+
+
+#Challenge No3 
+# Create a program to find the sum of all elements in a list.
+
+def sum_func(n):
+    total=0
+    for i in n:
+        total+=i
+    return total
+
+user_input=input('enter your numbers')
+numbers=[int(x) for x in user_input.split()]
+print(numbers)
+result=sum_func(numbers)
+print(result)
